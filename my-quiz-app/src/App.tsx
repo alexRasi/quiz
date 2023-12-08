6fea180f3d1abe570c43aa4caccfd40070e45861
Alexandros Rasidakis <alex.rasi94@gmail.com>
2023-12-08 23:03:51 +0100

    add fonts

--- my-quiz-app/src/App.tsx
+++ my-quiz-app/src/App.tsx
@@ -2,13 +2,16 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import './fonts.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-     test
+     <h1>Ερώτηση Φυσικής Πανελληνίων</h1>
+
+<p>Ποιος νόμος της φυσικής περιγράφει τη σχέση μεταξύ της δύναμης που ασκείται σε ένα αντικείμενο, της μάζας του αντικειμένου και της επιτάχυνσής του;</p>
     </>
   )
 }
